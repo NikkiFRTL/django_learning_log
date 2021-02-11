0c7a4c20b2636e4b37955f1d975872844cd8fbf1
NikkiFRTL <Mumkiss74>
2021-02-11 23:09:10 +0500

    Commit #1 from work

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,7 +10,11 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+
     # Страница со списком всех тем.
     path('topics', views.topics, name='topics')
+
+    # Страница с подробной информацией по отдельной теме.
+    path('topics/<int:topic_id>/', views.topics, name='topic'),
 
 ]
