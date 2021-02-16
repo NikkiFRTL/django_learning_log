9efe489f0e082e693dcf4b086f27463a1f2bce66
NikkiFRTL <70467189+NikkiFRTL@users.noreply.github.com>
2021-02-16 10:50:02 +0500

    Commit #5

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,11 @@
+"""
+Представление topics
+Функция topics() должна получать данные из базы данных и отправлять их шаблону.
+"""
+
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic  # Сначала импортируется модель, связанная с нужными данными
+
 
 # Create your views here.
 def index(request):
@@ -9,8 +15,16 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
-def topics(request):
+def topics(request):  # Функции topics() необходим один параметр: объект request, полученный Django от сервера.
+    """
+    Выводит список тем.
+    """
+    # Далеев ыдается запрос к базе данных на получение объектов Topic, отсортированных по атрибуту date_added.
+    # Полученный итоговый набор сохраняется в topics.
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
-    return render(request, 'learning_logs/topics.html', context)
+    return render(request, 'learning_logs/topics.html', context)  # Контекст.
+    # Контекст представляет собой словарь,
+    # в котором ключами являются имена, используемые в шаблоне для обращения к данным,
+    # а значениями — данные, которые должны передаваться шаблону.
 
